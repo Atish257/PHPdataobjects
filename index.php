ab60e69758ca2c0ad2afb59ab728a1ce8b4efe1c
atish anand nayak <an478@afsconnect2.njit.edu>
2017-11-02 21:37:23 -0400

    Number of records displayed after the query is displayed

--- index.php
+++ index.php
@@ -7,10 +7,40 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=an478", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
+    echo "SQL Database connected successfully<br><hr><br>"; 
     }
 catch(PDOException $e)
     {
-    echo "Connection failed: " . $e->getMessage();
+    echo "Connection failed. Try connecting once again !" . $e->getMessage();
     }
+
+
+function exeQuery($query) 
+{
+ 	global $conn;
+    try 
+    {
+ 	  $q = $conn->prepare($query);//prepare the query for execution
+ 	  $q->execute();//execute
+ 	  $answers = $q->fetchAll();//fecth the results of execution
+ 	  $q->closeCursor();
+ 	  return $answers;	
+    }
+    catch (PDOException $e) 
+    {
+ 	echo "Check for internal server error" . $e->getMessage();
+    }	  
+ }
+
+
+ $query2 = "select * from accounts where id<6";
+ $result2 = exeQuery($query2);
+
+ echo "The number of records in Accounts are: ";
+ print_r(count($result2));
+ echo "<br><hr><br>";
+
+
+ $conn = null;
  ?>
+
